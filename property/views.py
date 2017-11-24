58060e2f9296dbcdbaeb0d4971a7aa2d7f3ed850
Joe Pelz <joe.pelz@gmail.com>
2017-11-24 11:23:20 -0800

    placeholder views

--- property/views.py
+++ property/views.py
@@ -2,5 +2,41 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def index(request):
+def home(request):
     return HttpResponse("Index page. List of recently added properties.")
+
+def search_results(request):
+    return HttpResponse("Search page. Specify filters and see results.")
+
+def search(request):
+    return HttpResponse("headless. get search results")
+
+def details(request):
+    return HttpResponse("show details of property")
+
+def new(request):
+    return HttpResponse("form to create new property listing")
+
+def create(request):
+    return HttpResponse("headless (from new) - create new property")
+
+def edit(request):
+    return HttpResponse("form to edit property listing")
+
+def update_listing(request):
+    return HttpResponse("headless (from edit) - update property details")
+
+def remove(request):
+    return HttpResponse("delete a listing")
+
+def login(request):
+    return HttpResponse("Login via rioauth (opt. via google or github)")
+
+def logout(request):
+    return HttpResponse("headless. logout and redirect to home")
+
+def user(request):
+    return HttpResponse("Show/edit contact info and this user's listed properties.")
+
+def update_user(request):
+    return HttpResponse()
